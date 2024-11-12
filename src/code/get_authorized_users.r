fb6d804603d111a3928807470cf6d0d082cd7560
Subekshya Bidari <37636707+sbidari@users.noreply.github.com>
2024-11-12 14:13:39 -0500

    auto merge validated PRs (#32)

---
+++ src/code/get_authorized_users.r
@@ -0,0 +1,36 @@
+
+hub_path <- "."
+output_path <- "auxiliary-data/"
+
+yml_files <- list.files(file.path(hub_path, "model-metadata"),
+  pattern = "\\.ya?ml$", full.names = TRUE
+)
+
+extract_metadata <- function(file) {
+  yml_data <- yaml::yaml.load_file(file)
+  team_abbr <- ifelse("team_abbr" %in% names(yml_data), yml_data$team_abbr, NA)
+  model_abbr <- ifelse(
+    "model_abbr" %in% names(yml_data), yml_data$model_abbr, NA
+  )
+  designated_user <- ifelse(
+    "designated_github_user" %in% names(yml_data),
+    paste(yml_data$designated_github_user, collapse = ", "),
+    NA
+  )
+
+  return(list(
+    team_abbr = team_abbr,
+    model_abbr = model_abbr,
+    designated_github_users = designated_user
+  ))
+}
+
+metadata_list <- purrr::map(yml_files, extract_metadata)
+data_df <- do.call(rbind, lapply(metadata_list, as.data.frame))
+colnames(data_df) <- c("team_name", "model_name", "designated_users")
+
+output <- glue::glue(
+  "{data_df$team_name}-{data_df$model_name} {data_df$designated_users}"
+)
+
+writeLines(output, file.path(output_path, "authorized_users.txt"))
