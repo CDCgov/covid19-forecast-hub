9fec3f8c3b2a4da902de707d5fa7888205440d25
upx3 (CFA) <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-12-06 17:59:09 -0500

    Generalize Reference Data Exclusion Logic (#146)
    * initial commit, ask file type
    
    * begin shift to TOML based exclusions
    
    * correct exclusions
    
    * final removal of original code
    
    * Update src/code/gen_map_data.R
    
    Co-authored-by: Subekshya Bidari <37636707+sbidari@users.noreply.github.com>
    
    * Update src/code/gen_forecast_data.R
    
    Co-authored-by: Subekshya Bidari <37636707+sbidari@users.noreply.github.com>
    
    * remove json file for excluded locs
    
    ---------
    
    Co-authored-by: Subekshya Bidari <37636707+sbidari@users.noreply.github.com>

--- src/code/gen_map_data.R
+++ src/code/gen_map_data.R
@@ -92,23 +92,22 @@ if (length(missing_pop_columns) > 0) {
   stop(paste("Missing columns in population data:", paste(missing_pop_columns, collapse = ", ")))
 }
 
-# check if the reference date is the first
-# week for the season (2024-11-23), if so
-# exclude some locations
-if (ref_date == "2024-11-23") {
-  exclude_data_path <- fs::path(
-    base_hub_path, 
-    "auxiliary-data", 
-    paste0(ref_date, "-exclude-locations.json"))
-  if (!fs::file_exists(exclude_data_path)) {
-    stop("Exclude locations file not found: ", exclude_data_path)
+# check if the reference date has any
+# exclusions and exclude specified locations if any
+exclude_data_path_toml <- fs::path(base_hub_path, "auxiliary-data", "excluded_locations.toml")
+if (fs::file_exists(exclude_data_path_toml)) {
+  exclude_data_toml <- RcppTOML::parseTOML(exclude_data_path_toml)
+  if (ref_date %in% names(exclude_data_toml)) {
+    excluded_locations <- exclude_data_toml[[ref_date]]
+    message("Excluding locations for reference date: ", ref_date)
+  } else {
+    excluded_locations <- character(0)
+    message("No exclusion for reference date: ", ref_date)
   }
-  exclude_data <- jsonlite::fromJSON(exclude_data_path)
-  excluded_locations <- exclude_data$locations
-  message("Excluding locations for ref_date ", ref_date)
 } else {
-  excluded_locations <- character(0)
+  stop("TOML file not found: ", exclude_data_path_toml)
 }
+
 
 # process ensemble data into the required 
 # format for Map file
