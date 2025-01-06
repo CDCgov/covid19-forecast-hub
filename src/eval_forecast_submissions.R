90024fb102f4a5489623a02f79dc370fbb3d538d
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2025-01-06 12:05:06 -0500

    mixed-progress; hub_to_scorable_quantiles seems to work

--- src/eval_forecast_submissions.R
+++ src/eval_forecast_submissions.R
@@ -1,14 +1,16 @@
-#' This script evaluates all COVID-Hub
-#' submissions for a given reference date
-#' using the package scoringutils.
+#' This script evaluates all (minus provided
+#' exceptions) COVID-Hub submissions for a
+#' given reference date using the package
+#' scoringutils. The model submissions for
+#' a given reference date are grouped.
 #'
 #' To run:
-#' Rscript eval_forecast_submissions --target_data TRUE \
-#'   --reference_date 2024-11-23 --base_hub_path ../
+#' Rscript eval_forecast_submissions.R
+#' --reference_date 2024-11-23 --base_hub_path ../
 
 
 parser <- argparser::arg_parser(
-  "Command line parser for evaluating submissions to the COVID-Hub."
+  "Command line parser for evaluating model submissions to the COVID-Hub."
 )
 parser <- argparser::add_argument(
   parser,
@@ -29,9 +31,62 @@ parser <- argparser::add_argument(
   help = "Which models to exclude from the evaluation of submissions."
 )
 
+
+
+# collect parsed CLI arguments
+args <- argparser::parse_args(parser)
+base_hub_path <- args$base_hub_path
+reference_date <- args$reference_date
+
+# model_output_path <-
+
+hub_table <- forecasttools::hub_to_scorable_quantiles(base_hub_path)
+
+# get all model names (minus exclusions) to
+# consider
+
+# function for getting all forecast files
+# for a particular model directory
+read_forecast_data <- function(model_dir, reference_date) {
+  forecast_files <- list.files(
+    model_dir,
+    pattern = paste0(reference_date, ".*\\.csv"),
+    full.names = TRUE
+  )
+  forecasts <- purrr::map_dfr(forecast_files, readr::read_csv)
+  return(forecasts)
+}
+
+
+# function for getting target observation
+# data from target-data
+read_observation_data <- function(target_data_path) {
+  observed <- readr::read_csv(target_data_path) |>
+    dplyr::rename(target_end_date = date)
+  return(observed)
+}
+
+
+# accumulate
+read_model_csvs <- function(model_dir) {
+  files <- list.files(
+    file.path(
+      base_path,
+      model_dir
+    ),
+    pattern = "*.csv", full.names = TRUE
+  )
+  df <- purrr::map_df(files, function(x) {
+    readr::read_csv(x, show_col_types = FALSE) %>%
+      mutate(model = model_dir)
+  })
+  return(df)
+}
+
+
 score_hubverse <- function(forecast,
                            observed,
-                           horizons = c(0, 1),
+                           horizons = c(0, 1, 2),
                            transform = scoringutils::log_shift,
                            append_transformed = FALSE,
                            offset = 1,
