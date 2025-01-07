6c00fc188ffdd148a5a4cf2fbf3b1cb224b05dc7
Dylan H. Morris <dylan@dylanhmorris.com>
2025-01-06 21:10:32 -0500

    eval plots with exclusions

--- src/eval_forecast_submissions.R
+++ src/eval_forecast_submissions.R
@@ -6,70 +6,50 @@
 #'
 #' To run:
 #' Rscript eval_forecast_submissions.R
-#' --reference_date 2024-11-23 --base_hub_path ../
 
 
-parser <- argparser::arg_parser(
-  "Command line parser for evaluating model submissions to the COVID-Hub."
-)
-parser <- argparser::add_argument(
-  parser,
-  "--reference_date",
-  type = "character",
-  help = "The reference date for the forecast in YYYY-MM-DD format (ISO-8601)."
-)
-parser <- argparser::add_argument(
-  parser,
-  "--scores_as_of",
-  type = "character",
-  help = "The date upon which scores were measured in YYYY-MM-DD format (ISO-8601)."
-)
-parser <- argparser::add_argument(
-  parser,
-  "--base_hub_path",
-  type = "character",
-  help = "Path to the COVID-19 forecast hub directory."
-)
-# TODO: operate in the same fashion as w/ TOML
-# (1) yes, continue to exclude by locations
-# (2) also, have possibility for exclusion by model
+get_date_specific_exclusions <- function(base_hub_path) {
+    exclusions <- RcppTOML::parseTOML(fs::path("auxiliary-data",
+                                               "excluded_locations",
+                                               ext = "toml")) |>
+        tibble::enframe(name = "reference_date",
+                        value = "location") |>
+        dplyr::mutate(
+                   reference_date = lubridate::ymd(.data$reference_date)) |>
+        tidyr::unnest_longer("location")
 
-# parse cli arguments
-args <- argparser::parse_args(parser)
-base_hub_path <- args$base_hub_path
-scores_as_of_date <- args$scores_as_of
-reference_date <- args$reference_date
-models_to_exclude <- args$model_to_exclude
-
-# generate a table of scorable quantiles
-hub_table <- forecasttools::hub_to_scorable_quantiles(
-  hub_path = base_hub_path,
-  target_data_rel_path = fs::path("target-data", "covid-hospital-admissions.csv")
-)
-
-# perform scoring
-scored_results <- scoringutils::score(
-  hub_table,
-  metrics = scoringutils::get_metrics(hub_table)
-)
-
-# save the scoring results (unsummarized table)
-output_path <- fs::path(base_hub_path, "eval-output")
-base::dir.create(output_path, showWarnings = FALSE)
-scoring_output_file <- fs::path(output_path, base::paste0(scores_as_of_date, ".csv"))
-
-# check if the file exists
-if (!fs::file_exists(scoring_output_file)) {
-  readr::write_csv(scored_results, scoring_output_file)
-  base::cat("File written to:", scoring_output_file, "\n")
-} else {
-  base::cat("File already exists. Skipping write:", scoring_output_file, "\n")
+    return(exclusions)
 }
 
-# load scores for postprocessing
-quantile_scores <- scored_results
+get_excluded_locs <- function(base_hub_path) {
+    ex_locs <- RcppTOML::parseTOML(fs::path("auxiliary-data",
+                                         "excluded_territories",
+                                         ext = "toml"))$locations
 
-print(quantile_scores)
+    return(ex_locs)
+}
+
+
+get_hub_table <- function(base_hub_path) {
+    exclusions <- get_date_specific_exclusions(base_hub_path)
+    always_excluded_locs <- get_excluded_locs(base_hub_path)
+    target_data_rel_path <- fs::path("target-data",
+                                     "covid-hospital-admissions.csv")
+    hub_table <- forecasttools::hub_to_scorable_quantiles(
+                                    hub_path = base_hub_path,
+                                    target_data_rel_path =
+                                        target_data_rel_path) |>
+        dplyr::filter(.data$horizon >= 0,
+                      !.data$location %in% !!always_excluded_locs) |>
+        dplyr::anti_join(exclusions,
+                         by = c("reference_date", "location")) |>
+        dplyr::rename(model = "model_id") |>
+        dplyr::mutate(location = forecasttools::us_loc_code_to_abbr(
+                                                    .data$location))
+
+    return(hub_table)
+}
+
 
 with_horizons <- function(df) {
   return(df |>
@@ -90,24 +70,23 @@ summarised_scoring_table <- function(quantile_scores,
       by = by
     ) |>
     dplyr::filter(.data$compare_against == !!baseline) |>
-    dplyr::select(model_id,
+    dplyr::select("model",
       dplyr::all_of(by),
       relative_wis =
         "wis_scaled_relative_skill"
     )
 
   summarised <- filtered_scores |>
-    scoringutils::summarise_scores(by = c("model_id", by)) |>
-    dplyr::select(model_id,
+    scoringutils::summarise_scores(by = c("model", by)) |>
+    dplyr::select("model",
       dplyr::all_of(by),
-      abs_wis = wis,
-      mae = ae_median,
-      interval_coverage_50,
-      interval_coverage_90,
-      interval_coverage_95
+      abs_wis = "wis",
+      mae = "ae_median",
+      "interval_coverage_50",
+      "interval_coverage_90"
     ) |>
     dplyr::inner_join(summarised_rel,
-      by = c("model_id", by)
+      by = c("model", by)
     )
   return(summarised)
 }
@@ -116,7 +95,7 @@ summarised_scoring_table <- function(quantile_scores,
 plot_scores_by_date <- function(scores_by_date,
                                 date_column = "reference_date",
                                 score_column = "relative_wis",
-                                model_column = "model_id",
+                                model_column = "model",
                                 plot_title = "Scores by model over time",
                                 xlabel = "Date",
                                 ylabel = "Relative WIS") {
@@ -152,9 +131,9 @@ plot_scores_by_date <- function(scores_by_date,
 }
 
 relative_wis_by_location <- function(summarised_scores,
-                                     model_id = "CovidHub-ensemble") {
+                                     model = "CovidHub-ensemble") {
   summarised_scores <- summarised_scores |>
-    dplyr::filter(.data$model_id == !!model_id)
+    dplyr::filter(.data$model == !!model)
 
   min_wis <- base::min(summarised_scores$relative_wis)
   max_wis <- base::max(summarised_scores$relative_wis)
@@ -173,9 +152,9 @@ relative_wis_by_location <- function(summarised_scores,
     )) |>
     ggplot2::ggplot(
       ggplot2::aes(
-        y = location,
-        x = relative_wis,
-        group = model_id
+        y = .data$location,
+        x = .data$relative_wis,
+        group = .data$model
       )
     ) +
     ggplot2::geom_point(
@@ -202,6 +181,7 @@ coverage_plot <- function(data,
                           coverage_level,
                           date_column = "date") {
   coverage_column <- glue::glue("interval_coverage_{100 * coverage_level}")
+  print(data)
   return(
     ggplot2::ggplot(
       data = data,
@@ -217,7 +197,7 @@ coverage_plot <- function(data,
         linewidth = 1.5,
         linetype = "dashed"
       ) +
-      ggplot2::facet_wrap(~horizon_name) +
+      ggplot2::facet_wrap(c("horizon")) +
       ggplot2::scale_y_continuous(label = scales::label_percent()) +
       ggplot2::scale_x_date() +
       ggplot2::coord_cartesian(ylim = c(0, 1)) +
@@ -225,121 +205,112 @@ coverage_plot <- function(data,
   )
 }
 
-# postprocess scores
-summarised_scores <- summarised_scoring_table(
-  quantile_scores,
-  scale = "log",
-  baseline = "CovidHub-baseline"
-)
 
-print(summarised_scores)
+evaluate_and_save <- function(base_hub_path,
+                              scores_as_of_date) {
+    base_hub_path <- fs::path(base_hub_path)
+    hub_table <- get_hub_table(base_hub_path)
+    scored_results <- hub_table |>
+        scoringutils::score(metrics = scoringutils::get_metrics(hub_table))
 
-# save summarised scores
-summary_save_path <- fs::path(output_path, "summary_scores.tsv")
-readr::write_tsv(summarised_scores, summary_save_path)
+    output_path <- fs::path(base_hub_path, "eval-output")
+    fs::dir_create(output_path)
+    scoring_output_file <- fs::path(output_path,
+                                    glue::glue("{scores_as_of_date}.csv"))
 
-# generate and save coverage plots
-coverage_plots <- purrr::map(
-  c(0.5, 0.9, 0.95),
-  \(level) {
-    coverage_plot(
-      summarised_scores,
-      coverage_level = level,
-      date_column = "reference_date"
+    readr::write_csv(scored_results, scoring_output_file)
+    message("Raw scores written to ", scoring_output_file)
+    
+    summarised_scores <- summarised_scoring_table(
+        scored_results,
+        scale = "log",
+        baseline = "CovidHub-baseline"
     )
-  }
+
+    summarised_by_ref_date_horizon <- summarised_scoring_table(
+        scored_results,
+        scale = "log",
+        baseline = "CovidHub-baseline",
+        by = c("horizon", "reference_date", "target_end_date")
+    )
+
+    summarised_by_location_horizon <- summarised_scoring_table(
+        scored_results,
+        scale = "log",
+        baseline = "CovidHub-baseline",
+        by = c("horizon", "location")
+    )
+
+    summary_save_path <- fs::path(output_path, "summary_scores.tsv")
+    readr::write_tsv(summarised_scores, summary_save_path)
+
+    coverage_plots <- purrr::map(c(0.5, 0.9),
+                                 \(level) {
+                                     coverage_plot(
+                                         summarised_by_ref_date_horizon |>
+                                         dplyr::filter(model == "CovidHub-ensemble"),
+                                         coverage_level = level,
+                                         date_column = "target_end_date"
+                                     )
+                                 }
+                                 )
+    forecasttools::plots_to_pdf(
+                       coverage_plots,
+                       fs::path(output_path, "coverage_by_date_and_horizon.pdf"),
+                       width = 8,
+                       height = 4
+                   )
+
+    rel_wis_by_date <- plot_scores_by_date(
+        summarised_by_ref_date_horizon,
+        date_column = "reference_date",
+        score_column = "relative_wis",
+        model_column = "model"
+    )
+    ggplot2::ggsave(
+                 fs::path(output_path, "relative_wis_by_date.pdf"),
+                 rel_wis_by_date,
+                 width = 10,
+                 height = 8
+             )
+    
+
+
+    rel_wis_by_location_horizon <- relative_wis_by_location(
+        summarised_by_location_horizon,
+        model = "CovidHub-ensemble"
+    )
+    ggplot2::ggsave(
+                 fs::path(output_path, "relative_wis_by_location_horizon.pdf"),
+                 rel_wis_by_location_horizon,
+                 height = 10,
+                 width = 8
+             )
+    message(paste0("Scoring and plotting complete. ",
+                   "Outputs saved to "), output_path)
+}
+
+
+parser <- argparser::arg_parser(
+  "Command line parser for evaluating model submissions to the COVID-Hub."
 )
-forecasttools::plots_to_pdf(
-  coverage_plots,
-  fs::path(output_path, "coverage_by_date_and_horizon.pdf"),
-  width = 8,
-  height = 4
+parser <- argparser::add_argument(
+  parser,
+  "--scores-as-of",
+  type = "character",
+  default = lubridate::today(),
+  help = "The date upon which scores were measured in YYYY-MM-DD format (ISO-8601)."
+)
+parser <- argparser::add_argument(
+  parser,
+  "--base-hub-path",
+  type = "character",
+  default = ".",
+  help = "Path to the COVID-19 forecast hub directory."
 )
 
-# generate and save relative wis by date plot
-rel_wis_by_date <- plot_scores_by_date(
-  summarised_scores,
-  date_column = "reference_date",
-  score_column = "relative_wis",
-  model_column = "model_id"
-)
-ggplot2::ggsave(
-  fs::path(output_path, "relative_wis_by_date.pdf"),
-  rel_wis_by_date,
-  width = 10,
-  height = 8
-)
-
-
-
-# generate and save relative wis by location and horizon plot
-rel_wis_by_location_horizon <- relative_wis_by_location(
-  summarised_scores,
-  model_id = "CovidHub-ensemble"
-)
-ggplot2::ggsave(
-  fs::path(output_path, "relative_wis_by_location_horizon.pdf"),
-  rel_wis_by_location_horizon,
-  height = 10,
-  width = 8
-)
-base::cat("Scoring and plotting complete. Outputs saved to: ", output_path, "\n")
-
-
-
-
-# https://github.com/CDCgov/pyrenew-hew/blob/main/pipelines/summarize_visualize_scores.R
-# in place of pyrenew-hew, we would do
-# COVID-Hub ensemble for this repository; we
-# want coverage for each sub-model of the
-# ensemble
-
-# TODO: use summarizes scores; DHM advocates
-# replicate [coverage plots],
-
-# # TODO: change once TOML read exclusions
-# # get hub table output
-# hub_table <- hub_table |>
-#   dplyr::filter(model_id %in% models_to_evaluate)
-# # filter out excluded models
-# all_models <- unique(hub_table$model_id)
-# if (!is.null(models_to_exclude)) {
-#   models_to_evaluate <- setdiff(all_models, models_to_exclude)
-# } else {
-#   models_to_evaluate <- all_models
-# }
-
-
-# # plot predictions vs observed data
-# # TODO: not state by state and date? DHM
-# # suggests commenting out for now, them
-# # one page per (model, state);
-# # tidyr::crossing() would be used here
-# model_ids <- unique(hub_table$model_id)
-# model_scores <- purrr::map(
-#   model_ids,
-#   \(model) {
-#     hub_table |>
-#     dplyr::filter(model_id == !!model) |>
-#     forecasttools::plot_pred_obs_by_forecast_date(
-#       horizons = c(0, 1, 2),
-#       prediction_interval_width = 0.95,
-#       forecast_date_col = "reference_date",
-#       target_date_col = "target_end_date",
-#       predicted_col = "predicted",
-#       observed_col = "observed",
-#       quantile_level_col = "quantile_level",
-#       horizon_col = "horizon",
-#       x_label = "Date",
-#       y_label = "Target",
-#       y_transform = "log10")
-# })
-# forecasttools::plots_to_pdf(
-#   model_scores,
-#   save_path = fs::path(
-#     base_hub_path,
-#     "eval-output",
-#     paste0(
-#       scores_as_of_date,
-#       "-model-scores.pdf"))) |>
-#   suppressMessages()
+args <- argparser::parse_args(parser)
+base_hub_path <- args$base_hub_path
+scores_as_of_date <- args$scores_as_of
+evaluate_and_save(args$base_hub_path,
+                  args$scores_as_of)
