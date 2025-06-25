f3ad7dd962a0493e1ccc88ccb35977ce2ccdac40
upx3 (CFA) <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2025-06-25 14:45:21 -0400

    Update src/get_authorized_users.R
    Co-authored-by: Dylan H. Morris <dylanhmorris@users.noreply.github.com>

--- src/get_authorized_users.R
+++ src/get_authorized_users.R
@@ -17,7 +17,7 @@ json_list <- purrr::map(yml_files, function(file) {
   team <- if (!is.null(y$team_abbr)) y$team_abbr else NA_character_
   model <- if (!is.null(y$model_abbr)) y$model_abbr else NA_character_
   designated_users <- if (!is.null(y$designated_github_users)) {
-    I(y$designated_github_users)
+    I(y$designated_github_users)  # ensure single-user lists are rendered as JSON lists
   } else {
     NA
   }
